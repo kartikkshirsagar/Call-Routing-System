698a4d0a53a27eb16dff5cb5aee9574d51ae6365
kartikkshirsagar <kartikpkshirsagar@gmail.com>
2020-03-30 16:19:27 +0530

    init

---
+++ main.c
@@ -0,0 +1,122 @@
+//ESSENTIALS
+/*
+1)Lets assume a 2D world and each mobile and the base have their respective locations identified by 
+  their co-ordinates.Distance will be calculated as per co-ordinate geometry.
+2)Every base station has a coverage radius of 
+3) The data in index node would be the distance from some reference.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define kary 4
+#define capacity 4
+#define min_capacity ceil(capacity/2)
+#define INITSIZE 4
+#define COVERAGE_RADIUS 10
+typedef struct mobile_phone{
+  int id;
+  coordinate coordinates;
+}mobile;
+
+
+typedef struct {
+  int *array;
+  size_t used;
+  size_t size;
+} vector;
+
+typedef struct Index_node_type{
+    int data;
+    struct BPlus_Tree_Node* first;
+    struct BPlus_Tree_Node* second;
+    struct BPlus_Tree_Node* third;
+    struct BPlus_Tree_Node* fourth;
+    
+}indexNode;
+typedef struct data_node_type{
+    int data[4];
+    struct BPlus_Tree_Node* next;
+    struct BPlus_Tree_Node* prev;
+}dataNode;
+
+typedef struct BPlus_Tree_Node{
+    char tag;
+    union type{
+        struct data_node_type dataNode;
+        struct Index_node_type indexNode;
+    }type;
+
+}treeNode;
+
+typedef struct pair{
+    int x;
+    int y;
+}coordinate;
+
+void initVector(vector *a) {
+  a->array = (int *)malloc(INITSIZE * sizeof(int));
+  a->used = 0;
+  a->size = INITSIZE;
+}
+
+int distance(coordinate a,coordinate b)
+{
+    float retval = (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
+    retval=sqrt(retval);
+    return (int)retval;
+}
+void push_back(vector *a, int element) {
+  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+  // Therefore a->used can go up to a->size 
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = (int *)realloc(a->array, a->size * sizeof(int));
+  }
+  a->array[a->used] = element;
+  a->used++;
+}
+void removeAt(vector *a,int i)
+{
+    if(i>a->used || i<0)
+    {
+        return;
+    }
+    int j=i;
+    while(j<a->used)
+    {
+        a->array[j]=a->array[j+1];
+        j++;
+
+    }
+    a->used--;
+    if(a->used<=(a->size/2))
+    {
+        a->size=a->size/2;
+        a->array = (int *)realloc(a->array, a->size * sizeof(int));
+    }
+
+}
+void clear(vector *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
+}
+void printVector(vector *a){
+    int i;
+    for(i=0;i<a->used;i++)
+    {   
+        printf("%d ",a->array[i]);
+    }
+    //printf(" --%ld-- ",a->size);
+    printf("\n");
+}
+
+
+
+
+
+int main(){
+    
+
+}
